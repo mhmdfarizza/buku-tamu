0d54a9668de92a5277416118f0bb5f3ab13fd9af
M. FARIZ Z.A <mhmdfarizza@gmail.com>
2026-09-24 23:01:41 +0700

    tambahkan fitur upload gambar di halaman edit tamu

--- function.php
+++ function.php
@@ -51,6 +51,14 @@ function ubah_tamu($data)
     $no_hp       = htmlspecialchars($data["no_hp"]);
     $bertemu     = htmlspecialchars($data["bertemu"]);
     $kepentingan = htmlspecialchars($data["kepentingan"]);
+    $gambar      = htmlspecialchars($data['gambarLama']);
+
+    // cek apakah user pilih gambar baru atau tidak
+    if ($_FILES['gambar']['error'] === 4) {
+        $gambar = $gambarLama;
+    } else {
+        $gambar = uploadGambar();
+    }
 
     // Proses update ke database
     $query = "UPDATE buku_tamu SET 
@@ -58,7 +66,8 @@ function ubah_tamu($data)
                 alamat       = '$alamat',
                 no_hp        = '$no_hp',
                 bertemu      = '$bertemu',
-                kepentingan  = '$kepentingan'
+                kepentingan  = '$kepentingan',
+                gambar       = '$gambar'
               WHERE id_tamu = '$id'";
 
     mysqli_query($koneksi, $query);
